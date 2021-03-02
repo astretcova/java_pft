894a24990e287782b170bd0b1eaec3fc378a84b6
Marina Astrecova <yamshikova@list.ru>
2021-03-02 15:50:01 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,7 +1,28 @@
+import java.security.PublicKey;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Marina");
+
+    System.out.println("area square (3) =  " + areaSquare(3));
+    System.out.println("area triangle (2, 3) =  " + areaTriangle(2, 3));
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double areaSquare(double a) {
+
+    return a * a;
+  }
+
+  public static double areaTriangle(double a, double b) {
+    return a * b;
   }
 
 }
