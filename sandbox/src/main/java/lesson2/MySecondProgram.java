33bd111170a41c5baaf4cb41fdb1d4e2d6f3454f
Marina Astrecova <yamshikova@list.ru>
2021-03-03 21:47:58 +0300

    убрала static у метода distance из класса Poin

--- sandbox/src/main/java/lesson2/MySecondProgram.java
+++ sandbox/src/main/java/lesson2/MySecondProgram.java
@@ -5,7 +5,6 @@ public class MySecondProgram {
   public static void main(String[] args) {
     Point p1 = new Point(-1, 3);
     Point p2 = new Point(6, 2);
-    System.out.println("расстояния между точками = " + Point.distance(p1, p2));
-
+    System.out.println("Расстояния между точками = " + p1.distance(p2));
   }
 }
