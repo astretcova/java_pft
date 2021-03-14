5601e2eff69f237503f6601acf25f95e44a19a3b
Marina Astrecova <yamshikova@list.ru>
2021-03-14 17:26:27 +0300

    hw4_правки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ConactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ConactData.java
@@ -1,18 +1,24 @@
 package ru.stqa.pft.addressbook;
 
-public class MainData {
+public class ConactData {
   private final String firstname;
   private final String lastname;
   private final String email;
   private final String address2;
   private final String mobile;
+  private final String address;
+  private final String email2;
+  private final String byear;
 
-  public MainData(String firstname, String lastname, String email, String address2, String mobile) {
+  public ConactData(String firstname, String lastname, String email, String address2, String mobile, String address, String email2, String byear) {
     this.firstname = firstname;
     this.lastname = lastname;
     this.email = email;
     this.address2 = address2;
     this.mobile = mobile;
+    this.address = address;
+    this.email2 = email2;
+    this.byear = byear;
   }
 
   public String getFirstname() {
@@ -33,5 +39,17 @@ public class MainData {
 
   public String getMobile() {
     return mobile;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getByear() {
+    return byear;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreationAddressBook.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreationAddressBook.java
@@ -1,6 +1,5 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,8 +11,6 @@ public class CreationAddressBook {
   @BeforeClass(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/group.php");
     login("admin", "secret");
   }
 
@@ -29,8 +26,7 @@ public class CreationAddressBook {
   public void testCreationAddressBook() throws Exception {
 
     goToNewGroup();
-    fillGroupCreation(new MainData("Марина", "Астрецова", "astrecova.marina@gmail.com", "санкт-петербург", "89110034406"),
-            new SecondData("ул Федора Абрамова д.8", "mastretsova@rbc.ru", "1984"));
+    fillGroupCreation(new ConactData("Марина", "Астрецова", "astrecova.marina@gmail.com", "санкт-петербург", "89110034406", "ул Федора Абрамова д.8", "mastretsova@rbc.ru", "1984");
     submitGroup();
     returnToPage();
   }
@@ -45,33 +41,33 @@ public class CreationAddressBook {
     wd.findElement(By.linkText("home page")).click();
   }
 
-  private void fillGroupCreation(MainData mainData, SecondData secondData) {
+  private void fillGroupCreation(ConactData conactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(mainData.getFirstname());
+    wd.findElement(By.name("firstname")).sendKeys(conactData.getFirstname());
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(mainData.getLastname());
+    wd.findElement(By.name("lastname")).sendKeys(conactData.getLastname());
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(secondData.getAddress());
+    wd.findElement(By.name("address")).sendKeys(conactData.getAddress());
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(mainData.getEmail());
+    wd.findElement(By.name("email")).sendKeys(conactData.getEmail());
 
     wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(mainData.getAddress2());
+    wd.findElement(By.name("address2")).sendKeys(conactData.getAddress2());
 
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(mainData.getMobile());
+    wd.findElement(By.name("mobile")).sendKeys(conactData.getMobile());
     wd.findElement(By.name("email2")).click();
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(secondData.getEmail2());
+    wd.findElement(By.name("email2")).sendKeys(conactData.getEmail2());
     wd.findElement(By.name("bday")).click();
     wd.findElement(By.name("bday")).click();
     wd.findElement(By.name("bmonth")).click();
     wd.findElement(By.name("bmonth")).click();
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys(secondData.getByear());
+    wd.findElement(By.name("byear")).sendKeys(conactData.getByear());
   }
 
   private void goToNewGroup() {
@@ -82,6 +78,4 @@ public class CreationAddressBook {
   public void tearDown() throws Exception {
     wd.quit();
   }
-
-
-  }
+}
